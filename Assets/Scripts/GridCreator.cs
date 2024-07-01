f149e80268d102b0ec5f5d8ea161c9e71e5e9eb3
BilalKirez <bilal@sonicmob.co>
2024-07-01 17:13:46 +0300

    Added Scripts
    Added GridCreatorScript
    Added ObjectPoolScript

---
+++ Assets/Scripts/GridCreator.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridCreator : MonoBehaviour
+{
+    public int gridSize;
+    public GameObject squarePrefab;
+    public TMP_InputField inputField;
+    public Button createButton;
+    public ObjectPool objectPool;
+    private float cellScale;
+    private Camera mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+        inputField.onValueChanged.AddListener(delegate { SetGridSize(inputField.text); });
+        createButton.onClick.AddListener(delegate { CreateGrid(); });
+        SetGridSize(inputField.text);
+        CreateGrid();
+    }
+    public void SetGridSize(string text)
+    {
+        gridSize = int.Parse(text);
+    }
+    public void CreateGrid()
+    {
+        for (int i = transform.childCount - 1; i >= 0; i--)
+        {
+            Transform child = transform.GetChild(i);
+            objectPool.ReturnObject(child.gameObject);
+            //Destroy(child.gameObject);
+        }
+        CalculateCellSize();
+        PlaceCells();
+        //for (int x = 0; x < gridSize; x++)
+        //{
+        //    for (int y = 0; y < gridSize; y++)
+        //    {
+        //        Vector3 position = new Vector3(x, y, 0);
+        //        GameObject obj = objectPool.GetObject();
+        //        obj.transform.position = position;
+        //        obj.transform.SetParent(transform);
+        //        //Instantiate(squarePrefab, position, Quaternion.identity, transform);
+        //    }
+        //}
+    }
+    private void CalculateCellSize()
+    {
+        float screenHeight = 2f * mainCamera.orthographicSize;
+        float screenWidth = screenHeight * mainCamera.aspect;
+
+        cellScale = Mathf.Min(screenWidth / gridSize, screenHeight / gridSize);
+    }
+    private void PlaceCells()
+    {
+        float startX = -((gridSize - 1) * cellScale) / 2f;
+        float startY = -((gridSize - 1) * cellScale) / 2f;
+
+        for (int i = 0; i < gridSize; i++)
+        {
+            for (int j = 0; j < gridSize; j++)
+            {
+                Vector3 position = new Vector3(startX + i * cellScale, startY + j * cellScale, 0);
+                GameObject obj = objectPool.GetObject();
+                obj.transform.position = position;
+                obj.transform.SetParent(transform);
+                obj.transform.localScale = new Vector3(cellScale, cellScale, 1);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    public GameObject prefab;
+    public int initialPoolSize = 10;
+
+    private Queue<GameObject> pool = new Queue<GameObject>();
+
+    void Start()
+    {
+        for (int i = 0; i < initialPoolSize; i++)
+        {
+            GameObject obj = Instantiate(prefab, transform);
+            obj.SetActive(false);
+            pool.Enqueue(obj);
+        }
+    }
+
+    public GameObject GetObject()
+    {
+        if (pool.Count > 0)
+        {
+            GameObject obj = pool.Dequeue();
+            obj.SetActive(true);
+            return obj;
+        }
+        else
+        {
+            GameObject newObj = Instantiate(prefab);
+            newObj.SetActive(true);
+            return newObj;
+        }
+    }
+
+    public void ReturnObject(GameObject obj)
+    {
+        obj.SetActive(false);
+        pool.Enqueue(obj);
+        obj.transform.SetParent(transform);
+    }
+}
